10674811ba0b31ebd40d9281491bde435f1fba78
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-16 10:57:34 +0500

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Bilal Mehdi on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
